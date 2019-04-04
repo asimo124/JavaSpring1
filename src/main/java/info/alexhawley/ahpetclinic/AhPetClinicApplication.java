1c436c99bc345b0b5568532baa528e1978653034
asimo124 <asimo124@yahoo.com>
2019-04-04 10:47:37 -0500

    First Commit

---
+++ src/main/java/info/alexhawley/ahpetclinic/AhPetClinicApplication.java
@@ -0,0 +1,13 @@
+package info.alexhawley.ahpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AhPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AhPetClinicApplication.class, args);
+    }
+
+}
